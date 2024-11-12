ab2557e3e7f104f55c50e39ba1415374dbfa04cf
Abhishek G P <abhishekabhii287@gmail.com>
2024-11-12 14:55:47 +0530

    Add files via upload

---
+++ Spring-di/src/main/java/com/spring/Spring_di/App.java
@@ -0,0 +1,13 @@
+package com.spring.Spring_di;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+    public static void main( String[] args ){
+    	ClassPathXmlApplicationContext cpx = new ClassPathXmlApplicationContext("configure.xml");
+    	Employee e = (Employee)cpx.getBean("emp");
+    	System.out.println(e);
+    	System.out.println(e.getName());
+    	System.out.println(e.getExperience());
+    }
+}
